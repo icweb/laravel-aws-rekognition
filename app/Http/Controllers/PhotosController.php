ad52e3ec9fff6d66e0a05822be80310856a9dac0
Ian <ianconway@protonmail.com>
2018-09-09 18:38:30 -0400

    first commit

---
+++ app/Http/Controllers/PhotosController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Aws\Rekognition\RekognitionClient;
+use Illuminate\Http\Request;
+
+class PhotosController extends Controller
+{
+    public function showForm()
+    {
+        return view('form');
+    }
+
+    public function submitForm(Request $request)
+    {
+        $client = new RekognitionClient([
+            'credentials' => [
+                'key'    => env('AWS_ACCESS_KEY_ID'),
+                'secret' => env('AWS_SECRET_ACCESS_KEY')
+            ],
+            'region'    => 'us-west-2',
+            'version'   => 'latest'
+        ]);
+
+        $image = fopen($request->file('photo')->getPathName(), 'r');
+        $bytes = fread($image, $request->file('photo')->getSize());
+
+        if($request->input('type') === 'nudity')
+        {
+            // https://docs.aws.amazon.com/rekognition/latest/dg/moderation.html
+            // https://docs.aws.amazon.com/rekognition/latest/dg/API_DetectModerationLabels.html
+
+            $results = $client->detectModerationLabels(['Image' => ['Bytes' => $bytes], 'MinConfidence' => intval($request->input('confidence'))])['ModerationLabels'];
+
+            if(array_search('Explicit Nudity', array_column($results, 'Name')))
+            {
+                $message = 'This photo may contain nudity';
+            }
+            else
+            {
+                $message = 'This photo does not contain nudity';
+            }
+        }
+        else
+        {
+            // https://docs.aws.amazon.com/rekognition/latest/dg/text-detection.html
+            // https://docs.aws.amazon.com/rekognition/latest/dg/API_DetectText.html
+
+            $results = $client->detectText(['Image' => ['Bytes' => $bytes], 'MinConfidence' => intval($request->input('confidence'))])['TextDetections'];
+
+            $string = '';
+            foreach($results as $item)
+            {
+                if($item['Type'] === 'WORD')
+                {
+                    $string .= $item['DetectedText'] . ' ';
+                }
+            }
+
+            if(empty($string))
+            {
+                $message = 'This photo does not have any words';
+            }
+            else
+            {
+                $message = 'This photo says ' . $string;
+            }
+        }
+
+        request()->session()->flash('success', $message);
+
+        return view('form', ['results' => $results]);
+    }
+}
---
+++ resources/views/form.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel + AWS Rekognition</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+</head>
+<body>
+
+<div class="container">
+
+    <div class="jumbotron">
+        <h3>Laravel + AWS Rekognition SDK Integration</h3>
+        <p>This project demonstrates the integration of the AWS Rekognition SDK into a Laravel project.</p>
+        <a target="_blank" href="https://github.com/icweb/laravel-aws-rekognition" class="btn btn-default"><em class="fa fa-github"></em> View on GitHub</a>
+    </div>
+
+    @if(session('success'))
+        <div class="alert alert-success">
+            <div class="form-group">{{ session('success') }}</div>
+            <a href="/" class="btn btn-success">Try Again</a>
+        </div>
+    @endif
+
+    @if(isset($results))
+        {{ dd($results) }}
+    @else
+        <form action="{{ action('PhotosController@submitForm') }}" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="type">Action</label>
+                <select name="type" id="type" class="form-control">
+                    <option value="text">Read Text</option>
+                    <option value="nudity">Detect Nudity</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="confidence">Minimum Confidence</label>
+                <input type="number" id="confidence" name="confidence" class="form-control" value="50">
+            </div>
+            <div class="form-group">
+                <label for="photo">Upload a Photo</label>
+                <input type="file" name="photo" id="photo" class="form-control">
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Submit" class="btn btn-success btn-lg">
+            </div>
+        </form>
+    @endif
+
+</div>
+
+</body>
+</html>
